7decca5b6ab002398477d8a9c2e0552df5c72be1
macbook <macbook@localhost>
2022-09-06 09:15:55 +0300

    CW #24 with tests and TODO's comment for HW #24

---
+++ data-time/src/telran/time/NextFriday13.java
@@ -0,0 +1,17 @@
+package telran.time;
+
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAdjuster;
+
+public class NextFriday13 implements TemporalAdjuster {
+
+	@Override
+	public Temporal adjustInto(Temporal temporal) {
+		// TODO 
+		// Friday is ChronoField.DAY_OF_WEEK == DayOfWeek.FRIDAY.ordinal() + 1
+		// 13 day of month ChronoField.DAY_OF_MONTH == 13
+		
+		return null;
+	}
+
+}
